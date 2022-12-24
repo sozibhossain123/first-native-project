f1c9d071d1eff4f0ce423f3758f4b5b392f4c3a5
sozibhossain <sozibhossain69@gmail.com>
2022-12-24 17:33:32 +0600

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
+import React from "react";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { colors } from "./src/theme/colors";
+import { spacing } from "./src/theme/spacing";
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+
+
+
 
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontFamily: 'Inter_900Black', fontSize: 20 }}>Hello sozib first react native app</Text>
+      
+      <View>
+        <Text style={styles.sozib }>Hello</Text>
+        <Image style={{width: 200, height: 200, borderRadius: 25, marginTop: spacing[8]}} source={require('./assets/robin.jpg')} />
+        
+      </View>
+      
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: colors.darkorange,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  sozib: {
+    backgroundColor: '#000',
+    fontSize: 30,
+    color: '#fff',
+    textAlign: 'center'
+    
+  },
+  
+
+  
+
 });
